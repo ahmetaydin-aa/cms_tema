79031419326dcad823c33da47b53d985a9eb8224
Ahmet Aydın <ahmetaydin.1995@gmail.com>
2020-04-24 05:08:36 +0300

    HTML include functionality added.

--- js/main.js
+++ js/main.js
@@ -19,4 +19,34 @@ $(document).ready(() => {
       nav_escape_items.removeClass("collapsed");
     }
   });
+
+  includeHTML();
 });
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  z = document.getElementsByClassName("w3-include-html");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-url");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-url");
+          $(elmnt).removeClass("w3-include-html");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
